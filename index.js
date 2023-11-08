c1c4aeea398356117f783327d88ac45dd7edc0f5
afifa <2023afifa@gmail.com>
2023-11-08 10:44:18 +0600

    Created delete section for manage food

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete("/allfood/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.get("/user", async (req, res) => {
             const cursor = userCollection.find();
             const users = await cursor.toArray();
