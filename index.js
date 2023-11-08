3474952ca832f21efa8f8f15a0ade7ec7a4c6d60
afifa <2023afifa@gmail.com>
2023-11-08 16:57:48 +0600

    Created get request for a single id

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/request/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await requestCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post("/request", async (req, res) => {
             const newRequest = req.body;
             console.log(newRequest);
